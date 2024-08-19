0dc32fff4564ac94e045f9593d424eec796a7d62
전재연 <sunrox1107@gmail.com>
2024-08-19 14:18:23 +0900

    first commit

---
+++ src/main/java/com/example/instaclone_9room/InstaClone9roomApplication.java
@@ -0,0 +1,13 @@
+package com.example.instaclone_9room;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstaClone9roomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstaClone9roomApplication.class, args);
+    }
+
+}
