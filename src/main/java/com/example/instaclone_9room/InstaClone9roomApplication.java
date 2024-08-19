5c2f83e30853d38292518e0f8b4ce8a5d3f89126
전재연 <sunrox1107@gmail.com>
2024-08-19 16:21:23 +0900

    세큐리티 적용 및 ApiResponse

--- src/main/java/com/example/instaclone_9room/InstaClone9roomApplication.java
+++ src/main/java/com/example/instaclone_9room/InstaClone9roomApplication.java
@@ -2,8 +2,10 @@ package com.example.instaclone_9room;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class InstaClone9roomApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/example/instaclone_9room/apiPayload/code/BaseCode.java
@@ -0,0 +1,6 @@
+package com.example.instaclone_9room.apiPayload.code;
+
+public interface BaseCode {
+    public ReasonDTO getReason();
+    public ReasonDTO getReasonHttpStatus();
+}
