74de1f10ccb696fa65307307c7ef305d4a480f7b
전재연 <sunrox1107@gmail.com>
2024-08-21 20:55:50 +0900

    re

--- src/main/java/com/example/instaclone_9room/apiPayload/code/BaseCode.java
+++
@@ -1,6 +0,0 @@
-package com.example.instaclone_9room.apiPayload.code;
-
-public interface BaseCode {
-    public ReasonDTO getReason();
-    public ReasonDTO getReasonHttpStatus();
-}
--- src/main/java/com/example/instaclone_9room/service/UserCommandService.java
+++
@@ -1,5 +0,0 @@
-package com.example.instaclone_9room.service;
-
-public interface UserCommandService {
-    void logout(String refreshToken);
-}
