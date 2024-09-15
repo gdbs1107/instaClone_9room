ac212390bc4f532c00949a0f4dced401b3a08769
yunsful <bailey05280@naver.com>
2024-09-15 22:34:53 +0900

    feat : FeedPinned 엔티티 추가

---
+++ src/main/java/com/example/instaclone_9room/domain/feedEntity/FeedPinned.java
@@ -0,0 +1,29 @@
+package com.example.instaclone_9room.domain.feedEntity;
+
+import com.example.instaclone_9room.domain.UserEntity;
+import com.example.instaclone_9room.domain.baseEntity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class FeedPinned extends BaseEntity {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_entity_id")
+    private UserEntity userEntity;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "feed_id")
+    private Feed feed;
+}
