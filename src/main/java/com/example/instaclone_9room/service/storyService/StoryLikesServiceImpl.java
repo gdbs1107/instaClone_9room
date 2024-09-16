7f4795661541a08abdf17d5689f43740fff8eaea
yunsful <bailey05280@naver.com>
2024-09-16 17:37:10 +0900

    feat : 스토리 좋아요 api 추가

---
+++ src/main/java/com/example/instaclone_9room/service/storyService/StoryLikesServiceImpl.java
@@ -0,0 +1,58 @@
+package com.example.instaclone_9room.service.storyService;
+
+import com.example.instaclone_9room.domain.UserEntity;
+import com.example.instaclone_9room.domain.feedEntity.Feed;
+import com.example.instaclone_9room.domain.feedEntity.FeedLikes;
+import com.example.instaclone_9room.domain.storyEntitiy.Story;
+import com.example.instaclone_9room.domain.storyEntitiy.StoryLikes;
+import com.example.instaclone_9room.repository.UserRepository;
+import com.example.instaclone_9room.repository.storyRepository.StoryLikesRepository;
+import com.example.instaclone_9room.repository.storyRepository.StoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class StoryLikesServiceImpl implements StoryLikesService {
+    
+    private final StoryLikesRepository storyLikesRepository;
+    private final StoryRepository storyRepository;
+    private final UserRepository userRepository;
+    
+    @Override
+    public void toggleStoryLike(Long storyId, String username) {
+        UserEntity findUser = findUser(username);
+        
+        Story story = storyRepository.findById(storyId).orElseThrow(
+                () -> new RuntimeException("story not found"));
+        
+        
+        StoryLikes storyLikes = storyLikesRepository.findByUserEntityAndStory(findUser, story)
+                .orElse(null);
+        
+        if(storyLikes!=null){
+            
+            story.removeLike();
+            storyLikesRepository.delete(storyLikes);
+            
+        }else {
+            
+            StoryLikes newStoryLikes = StoryLikes.builder()
+                    .story(story)
+                    .userEntity(findUser)
+                    .build();
+            
+            story.addLike();
+            
+            storyLikesRepository.save(newStoryLikes);
+        }
+    }
+    
+    private UserEntity findUser(String username) {
+        return userRepository.findByUsername(username).orElseThrow(
+                () -> new RuntimeException("User not found")
+        );
+    }
+}
