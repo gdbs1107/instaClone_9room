b84698bf79c74b33a2ba693ef43a353ebb3da462
yunsful <bailey05280@naver.com>
2024-09-21 19:34:28 +0900

    feat : 하이라이트, 매핑테이블 엔티티 추가

---
+++ src/main/java/com/example/instaclone_9room/domain/storyEntitiy/StoryHighlight.java
@@ -0,0 +1,35 @@
+package com.example.instaclone_9room.domain.storyEntitiy;
+
+import com.example.instaclone_9room.domain.baseEntity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class StoryHighlight extends BaseEntity {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "story_highlight_id")
+    private Long id;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "story_id")
+    private Story story;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "highlight_id")
+    private Highlight highlight;
+    
+    public void setMapping(Story story, Highlight highlight) {
+        this.story = story;
+        this.highlight = highlight;
+        highlight.getStoryHighlights().add(this);
+    }
+}
