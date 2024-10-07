9486691fe3b6780cb7fc12ad066760b7473d0464
전재연 <sunrox1107@gmail.com>
2024-10-07 12:33:34 +0900

    [add] validate 어노테이션 추가를 위한 패키징

---
+++ src/main/java/com/example/instaclone_9room/validate/annotation/ExistMember.java
@@ -0,0 +1,12 @@
+package com.example.instaclone_9room.validate.annotation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+
+public @interface ExistMember {
+
+
+}
---
+++ src/main/java/com/example/instaclone_9room/validate/validator/ExistMemberValidator.java
@@ -0,0 +1,4 @@
+package com.example.instaclone_9room.validate.validator;
+
+public class ExistMemberValidator {
+}
