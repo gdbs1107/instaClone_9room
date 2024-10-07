597a676bc08550cb8046f80453cf0f1385856620
전재연 <sunrox1107@gmail.com>
2024-10-07 12:35:48 +0900

    [feat] 테스트 어노테이션 작성

--- src/main/java/com/example/instaclone_9room/validate/annotation/ExistMember.java
+++ src/main/java/com/example/instaclone_9room/validate/annotation/ExistMember.java
@@ -1,12 +1,19 @@
 package com.example.instaclone_9room.validate.annotation;
 
+import com.example.instaclone_9room.validate.validator.ExistMemberValidator;
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
 
 import java.lang.annotation.*;
 
 
+@Documented
+@Constraint(validatedBy = ExistMemberValidator.class)
+@Target( { ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER })
+@Retention(RetentionPolicy.RUNTIME)
 public @interface ExistMember {
-
+    String message() default "해당 하는 사용자가 존재 하지 않습니다.";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
 
 }
--- src/main/java/com/example/instaclone_9room/validate/validator/ExistMemberValidator.java
+++ src/main/java/com/example/instaclone_9room/validate/validator/ExistMemberValidator.java
@@ -1,4 +1,45 @@
 package com.example.instaclone_9room.validate.validator;
 
-public class ExistMemberValidator {
+import com.example.instaclone_9room.apiPayload.code.status.ErrorStatus;
+import com.example.instaclone_9room.repository.userEntityRepository.UserRepository;
+import com.example.instaclone_9room.validate.annotation.ExistMember;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class ExistMemberValidator implements ConstraintValidator<ExistMember, Long> {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public void initialize(ExistMember constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(Long userId, ConstraintValidatorContext context) {
+        boolean isValid;
+        ErrorStatus errorStatus;
+        // null is invalid
+        if (userId == null) {
+            errorStatus = ErrorStatus.MEMBER_NOT_FOUND;
+            isValid = false;
+        } else {
+            errorStatus = ErrorStatus.MEMBER_NOT_FOUND;
+            isValid = userRepository.findById(userId).isPresent();
+        }
+
+        if (!isValid) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(errorStatus.toString())
+                    .addConstraintViolation();
+        }
+
+        return isValid;
+    }
 }
